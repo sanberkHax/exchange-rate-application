35192b8d8a0212900c2d5de36d6ae98ab957bf0f
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-07-08 19:50:02 +0300

    Add error boundary

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router';
+import { ErrorBoundary } from 'react-error-boundary';
 
 function App() {
   return (
     <>
-      <Suspense
+      <ErrorBoundary
         fallback={
-          <div className="h-full w-full flex items-center justify-center">
-            <p>Loading...</p>
+          <div
+            className="text-red-500 w-screen h-screen flex flex-col justify-center items-center gap-4"
+            role="alert"
+          >
+            <h2 className="text-lg font-semibold">
+              {`Ooops, something went wrong :(`}
+            </h2>
+            <button
+              className=" bg-red-500 p-4 text-white"
+              onClick={() => window.location.assign(window.location.origin)}
+            >
+              Refresh
+            </button>
           </div>
         }
       >
-        <Outlet />
-      </Suspense>
+        <Suspense
+          fallback={
+            <div className="h-full w-full flex items-center justify-center">
+              <p>Loading...</p>
+            </div>
+          }
+        >
+          <Outlet />
+        </Suspense>
+      </ErrorBoundary>
     </>
   );
 }

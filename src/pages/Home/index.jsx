b4feaa68098b1d67817d44c5b652f1ba88604c44
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-07-09 19:59:19 +0300

    Add context to home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,12 @@
+import { useContext } from 'react';
 import { Navigate } from 'react-router-dom';
-import { storage } from '@/utils/storage';
+import { AuthContext } from '../../store/AuthContext';
 
 const Home = () => {
-  const token = storage.getToken();
+  const { isLoggedIn } = useContext(AuthContext);
   return (
     <>
-      {token ? (
+      {isLoggedIn ? (
         <div className="flex flex-col gap-10 justify-center items-center text-center">
           <h1 className="text-black font-bold text-3xl">Home</h1>
         </div>

f102275544f10ab81e1d11b14ae79639bedea53e
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-07-09 18:57:21 +0300

    Create dummy response

---
+++ src/constants/DummyResponse.js
@@ -0,0 +1,7 @@
+export const EXCHANGE_RATES = [
+  { currencyId: 2, midRate: 26.0616, nameEn: 'USD' },
+  { currencyId: 3, midRate: 28.3626, nameEn: 'EUR' },
+  { currencyId: 14, midRate: 33.218, nameEn: 'GBP' },
+  { currencyId: 13, midRate: 3.8061, nameEn: 'DKK' },
+  { currencyId: 15, midRate: 29.0973, nameEn: 'CHF' },
+];

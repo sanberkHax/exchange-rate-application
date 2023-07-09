afd07d3a9a1f0e1dbf749c7c2830ed684ff08f97
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-07-09 17:11:23 +0300

    Create utils for storage

---
+++ src/utils/storage.js
@@ -0,0 +1,13 @@
+const storage = {
+  getToken: () => {
+    return JSON.parse(window.localStorage.getItem('token'));
+  },
+  setToken: token => {
+    window.localStorage.setItem('token', JSON.stringify(token));
+  },
+  clearToken: () => {
+    window.localStorage.removeItem('token');
+  },
+};
+
+export default storage;

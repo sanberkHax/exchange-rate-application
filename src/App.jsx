31aa2dc01116d7868ad6100efb6729792f2f1638
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-07-09 19:37:11 +0300

    Change provider structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,20 @@
-import { Suspense } from 'react';
 import { Outlet } from 'react-router';
-import { ErrorBoundary } from 'react-error-boundary';
-import { ToastContainer } from 'react-toastify';
 import { Layout } from './components/Layout';
+import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
     <>
-      <ErrorBoundary
-        fallback={
-          <div
-            className="text-red-500 w-screen h-screen flex flex-col justify-center items-center gap-4"
-            role="alert"
-          >
-            <h2 className="text-lg font-semibold">
-              {`Ooops, something went wrong :(`}
-            </h2>
-            <button
-              className=" bg-red-500 p-4 text-white"
-              onClick={() => window.location.assign(window.location.origin)}
-            >
-              Refresh
-            </button>
-          </div>
-        }
-      >
-        <Suspense
-          fallback={
-            <div className="h-full w-full flex items-center justify-center">
-              <p>Loading...</p>
-            </div>
-          }
-        >
-          <Layout>
-            <Outlet />
-          </Layout>
-          <ToastContainer
-            position="top-left"
-            pauseOnFocusLoss={false}
-            autoClose={2000}
-            hideProgressBar
-            pauseOnHover={false}
-          />
-        </Suspense>
-      </ErrorBoundary>
+      <Layout>
+        <Outlet />
+      </Layout>
+      <ToastContainer
+        position="top-left"
+        pauseOnFocusLoss={false}
+        autoClose={2000}
+        hideProgressBar
+        pauseOnHover={false}
+      />
     </>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React, { lazy } from 'react';
+import React, { lazy, Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from '@/App.jsx';
 import '@progress/kendo-theme-default/dist/all.css';
@@ -7,6 +7,7 @@ import '@/index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Login from './pages/Login';
 import { AuthProvider } from './store/AuthContext';
+import { ErrorBoundary } from 'react-error-boundary';
 
 const Home = lazy(() => import('./pages/Home'));
 const NotFound = lazy(() => import('./pages/NotFound'));
@@ -29,7 +30,34 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
-      <RouterProvider router={router} />
+      <ErrorBoundary
+        fallback={
+          <div
+            className="text-red-500 w-screen h-screen flex flex-col justify-center items-center gap-4"
+            role="alert"
+          >
+            <h2 className="text-lg font-semibold">
+              {`Ooops, something went wrong :(`}
+            </h2>
+            <button
+              className=" bg-red-500 p-4 text-white"
+              onClick={() => window.location.assign(window.location.origin)}
+            >
+              Refresh
+            </button>
+          </div>
+        }
+      >
+        <Suspense
+          fallback={
+            <div className="h-full w-full flex items-center justify-center">
+              <p>Loading...</p>
+            </div>
+          }
+        >
+          <RouterProvider router={router} />
+        </Suspense>
+      </ErrorBoundary>
     </AuthProvider>
   </React.StrictMode>,
 );

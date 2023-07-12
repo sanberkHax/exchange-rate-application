ad27691ed46b3b2d988fddb86b95a70f49cf9dc4
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-07-12 03:22:55 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,12 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    colors: {
-      primary: '#0058e9',
-      secondary: '#03a9f4',
+    extend: {
+      colors: {
+        primary: '#0058e9',
+        secondary: '#03a9f4',
+      },
     },
-    extend: {},
   },
   plugins: [],
 };

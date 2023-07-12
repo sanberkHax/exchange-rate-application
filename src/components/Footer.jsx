752c927b09c0842b73f6f8fe06cce2238fd9cf56
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-07-12 03:22:41 +0300

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export const Footer = () => {
+  return (
+    <footer className="mt-10 sm:mt-0 p-2 sm:p-6 text-center font-bold bg-secondary text-white">
+      Exchange Rate Application
+    </footer>
+  );
+};
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import { Header } from './Header';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { useAuth } from '@/hooks/useAuth';
+import { Footer } from './Footer';
 
 export const Layout = () => {
   const auth = useAuth();
@@ -12,6 +13,7 @@ export const Layout = () => {
       <main className="min-h-[calc(100vh-110px)] flex flex-col items-center p-4 sm:p-10 gap-10">
         <Outlet />
       </main>
+      {auth.user && <Footer />}
       <ToastContainer
         position="top-left"
         pauseOnFocusLoss={false}

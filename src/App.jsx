75fa151dccec9a2ac9b421a8dd8f977f29abb1e6
Sanberk Türker <69405619+sanberkHax@users.noreply.github.com>
2023-07-12 04:51:06 +0300

    Add redirect from login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { lazy } from 'react';
 import Login from '@/pages/Login';
 import { Layout } from '@/components/Layout';
 import { ProtectedRoute } from '@/routes/ProtectedRoute';
+import { useAuth } from '@/hooks/useAuth';
 
 // Lazy load pages to improve performance
 const Home = lazy(() => import('@/pages/Home'));
 const NotFound = lazy(() => import('@/pages/NotFound'));
 
 const App = () => {
+  const auth = useAuth();
+
   return (
     <Routes>
       <Route element={<Layout />}>
@@ -20,7 +23,10 @@ const App = () => {
             </ProtectedRoute>
           }
         />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/login"
+          element={auth.user ? <Navigate to="/" /> : <Login />}
+        />
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
